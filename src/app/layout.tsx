3e0bb31c6c7fd618c36a93c5f15f63eb21abfdc8
Animashaun Samuel Oluwapelumi <Pelumis285@gmail.com>
2026-07-07 20:14:16 -0400

    Install Google Tag Manager container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { site } from "@/lib/site";
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-NMM23LS9";
+
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
   title: {
@@ -84,6 +86,23 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en-CA">
       <body className="flex min-h-screen flex-col bg-white font-sans text-slate-800 antialiased">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           id="ld-json"
           type="application/ld+json"
